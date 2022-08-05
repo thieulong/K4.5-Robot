9688eb503a2ea3f9f8669e43d2e4769e459c7e57
thieulong <thieulongph@gmail.com>
2022-08-05 20:49:57 +1000

    Done

--- console_control.py
+++ console_control.py
@@ -1,6 +1,9 @@
 from pynput import keyboard
 from lcd import write_lcd, clear_lcd
 from dc_motors import forward, backward, turn_left, turn_right, stop
+import cv2
+
+cam = cv2.VideoCapture(0)
 
 def on_press(key):
     
@@ -17,22 +20,32 @@ def on_press(key):
         elif key.char == 'd':
             turn_right()
             write_lcd(first_line=' MODE: CONSOLE', second_line=' Turning right')
+        elif key.char == 'z':
+            check, frame = cam.read()
+            cv2.imshow('video', frame)
+            cv2.waitKey(1)
+            cv2.waitKey(1)
+            write_lcd(first_line=' MODE: CONSOLE', second_line='Observing mode')
         
     except AttributeError:
         pass
 
 def on_release(key):
     
+    cam.release()
+    cv2.destroyAllWindows()
+    
     if key == keyboard.Key.esc:
         return False
     
     stop()
+    
     write_lcd(first_line=' MODE: CONSOLE', second_line='     READY')
     
 write_lcd(first_line=' MODE: CONSOLE', second_line='     READY')
+
 with keyboard.Listener(
         on_press=on_press,
         on_release=on_release) as listener:
     listener.join()
-    
-    
+
