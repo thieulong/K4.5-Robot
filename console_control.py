e5c3870fb68dd1a3348a9021ced81e045d2c3ed3
thieulong <thieulongph@gmail.com>
2022-08-06 15:46:14 +1000

    Added Safe-guard feature

--- console_control.py
+++ console_control.py
@@ -1,7 +1,13 @@
 from pynput import keyboard
 from lcd import write_lcd, clear_lcd
 from dc_motors import forward, backward, turn_left, turn_right, stop
+import message
 import cv2
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
 
 cam = cv2.VideoCapture(0)
 
@@ -31,12 +37,53 @@ def on_press(key):
         pass
 
 def on_release(key):
-    
-    cam.release()
+  
     cv2.destroyAllWindows()
     
     if key == keyboard.Key.esc:
         return False
+    
+    if key.char == 'x':
+        write_lcd(first_line='SAFE-GUARD MODE', second_line='   ACTIVATED')
+        with mp_pose.Pose(
+            min_detection_confidence=0.8,
+            min_tracking_confidence=0.8) as pose:
+        
+            while cam.isOpened():
+                success, image = cam.read()
+                image_hight, image_width, _ = image.shape
+            
+                if not success:
+                    print("Ignoring empty camera frame.")
+                    continue
+
+                image.flags.writeable = False
+                
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                results = pose.process(image)
+
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                
+                if results.pose_landmarks:
+                    write_lcd(first_line='    ALERT!', second_line='PERSON DETECTED!')
+                    x_cordinate = list()
+                    y_cordinate = list()
+                    for id, lm in enumerate(results.pose_landmarks.landmark):
+                        h, w, c = image.shape
+                        cx, cy = int(lm.x * w), int(lm.y * h)
+                        x_cordinate.append(cx)
+                        y_cordinate.append(cy)
+                        
+                    cv2.rectangle(img= image,
+                                pt1= (min(x_cordinate), max(y_cordinate)),
+                                pt2 = (max(x_cordinate), min(y_cordinate)-20),
+                                color= (0,0,255),
+                                thickness= 1)
+                    
+                    cv2.imwrite('person-detected/image.png', image)
+                    message.telegram(chat_id=message.telegram_chat_id)
+                    break
     
     stop()
     
